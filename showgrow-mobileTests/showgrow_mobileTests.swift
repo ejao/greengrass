be49dd1a39c49e8adcf18e25a6efe9b725867eb1
Edgar Jao <ejao@accuratebackground.com>
2017-09-24 18:21:44 -0700

    Initial Commit

---
+++ showgrow-mobileTests/showgrow_mobileTests.swift
@@ -0,0 +1,36 @@
+//
+//  showgrow_mobileTests.swift
+//  showgrow-mobileTests
+//
+//  Created by Edgar Jao on 9/24/17.
+//  Copyright © 2017 showgrow. All rights reserved.
+//
+
+import XCTest
+@testable import showgrow_mobile
+
+class showgrow_mobileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
